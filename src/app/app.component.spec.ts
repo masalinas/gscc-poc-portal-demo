afaa8cafa30e29b3800888059af2d9de2132ddcd
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2020-09-21 23:17:11 +0200

    last update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gscc-poc-parent'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('gscc-poc-parent');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
