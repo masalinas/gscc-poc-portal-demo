727f4fa560851023dd624d5ac70545a34c3f454a
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2020-09-21 21:02:17 +0200

    last update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, Router, RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
   { path: 'dashboard', loadChildren: () => import('./modules/dashboard/src/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'gsbi', loadChildren: () => import('./modules/gsbi/src/gsbi.module').then(m => m.GsbiModule) },
+  { path: 'gspl', loadChildren: () => import('./modules/gspl/src/gspl.module').then(m => m.GsplModule) },
 ];
 
 @NgModule({
@@ -10,4 +13,13 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {   
+  constructor(
+    private router: Router,
+  ) {
+    environment.modules.forEach(function(module) {
+        //console.log(module);
+        routes.push({ path: module, loadChildren: () => import('./modules/gsbi/src/gsbi.module').then(m => m.GsbiModule) });        
+      }
+    );
+  }
 }
